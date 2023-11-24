ff3fce2ad41aa3c3982f24634dccc4b474e2dbfa
DylanTaylor1 <89085119+DylanTaylor1@users.noreply.github.com>
2023-11-24 13:58:11 -0500

    Add files via upload

--- scan.lua
+++ scan.lua
@@ -6,18 +6,12 @@ local buffer = component.inventory_controller
 
 local function main()
 
-	-- Check Inventory Controller
-	if buffer == nil then
-		print('\nNo Inventory Controller Detected!\n')
-		return
-	end
-
 	print('\nITEM(S) IN BUFFER:')
 
 	for i=1, 99 do
 		local item = buffer.getStackInSlot(sides.up, i)
 		if item ~= nil then
-			print(string.format('label=%s name=%s damage=%s', item.label, item.name, item.damage))
+			print(string.format('label=%s // name=%s // damage=%s', item.label, item.name, item.damage))
 		else
 			break
 		end
